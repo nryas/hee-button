e9d75025fda0187fcc6d3f7064223005132f9117
Noriyasu Obushi <nryas@me.com>
2014-05-08 00:15:54 +0900

    bug fix test

--- server.js
+++ server.js
@@ -25,13 +25,13 @@ io.sockets.on('connection', function (socket) {
 	});
 
 	socket.on('clicked', function (data) {
-		count++;
+		count+=0.5;
 		console.log('個人'+data+', 全体'+count);
 
-		// // クリックした本人にのみ全体の回数を送信
-		// socket.emit('totalChanged', count, function (data) {
-		// 	console.log(data);
-		// });
+		// クリックした本人にのみ全体の回数を送信
+		socket.emit('totalChanged', count, function (data) {
+			console.log(data);
+		});
 
 		// それ以外のクライアントに全体の回数を送信
 		socket.broadcast.emit('totalChanged', count, function (data) {
