d4a55e41bcf9abfac01b58dc6553043c2f091f6e
Noriyasu Obushi <nryas@me.com>
2014-05-08 13:57:34 +0900

    revert前のcommit

--- server.js
+++ server.js
@@ -31,13 +31,13 @@ io.sockets.on('connection', function (socket) {
 	});
 
 	socket.on('clicked', function (data) {
-		count+=0.5;
+		count++;
 		console.log('個人'+data+', 全体'+count);
 
-		// クリックした本人にのみ全体の回数を送信
-		socket.emit('totalChanged', count, function (data) {
-			console.log(data);
-		});
+		// // クリックした本人にのみ全体の回数を送信
+		// socket.emit('totalChanged', count, function (data) {
+		// 	console.log(data);
+		// });
 
 		// それ以外のクライアントに全体の回数を送信
 		socket.broadcast.emit('totalChanged', count, function (data) {
