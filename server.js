a82316818d77f4574399a8893231bbe22d64024e
Noriyasu Obushi <nryas@me.com>
2014-11-18 13:34:42 +0900

    bug fix

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ io.sockets.on('connection', function (socket) {
 	});
 
 	socket.on('clicked', function (data) {
-		count+=0.5;
+		count+=1.0;
 		console.log('個人'+data+', 全体'+count);
 
 		// クリックした本人にのみ全体の回数を送信
